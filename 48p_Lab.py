fbf9d047954a049611f0dee7ba4b332f7752d1a1
Leona <102565567+alimhanhan@users.noreply.github.com>
2023-03-15 08:43:54 +0900

    Add files via upload

---
+++ 48p_Lab.py
@@ -0,0 +1,11 @@
+#파티 준비
+
+number = int(input("참석 인원:" ))
+
+chickens = number
+beers = number * 2
+cakes = number * 4
+
+print("치킨 개수: ", chickens)
+print("맥주 개수: ", beers)
+print("케이크 개수: ", cakes)
