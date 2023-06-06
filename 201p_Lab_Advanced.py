e2187bf2247e09f63ae2cd7760f55e1a5fe6c095
Leona <102565567+alimhanhan@users.noreply.github.com>
2023-06-06 12:51:58 +0900

    Add files via upload

---
+++ 201p_Lab_Advanced.py
@@ -0,0 +1,9 @@
+def birthday(name):
+    print('\n')
+    print('생일 축하합니다!')
+    print('생일 축하합니다!')
+    print('사랑하는 ', name, '의', sep='')
+    print('생일 축하합니다!')
+
+name_plz = input('이름: ')
+birthday(name_plz)
